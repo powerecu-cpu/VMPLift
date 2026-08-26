d15bd54cf4e5720aab6abb076be6194d06cd23ba
sexyiam <158042876+sexyiam@users.noreply.github.com>
2026-08-26 11:05:08 -0700

    Update vmp_version.hpp

--- vmp_version.hpp
+++ vmp_version.hpp
@@ -2,6 +2,9 @@
 
 #include "pe_image.hpp"
 
+#include <string>
+#include <vector>
+
 namespace vmp {
 
 enum class VmpVersion {
@@ -18,6 +21,14 @@ struct VersionInfo {
   bool has_merged_handlers = false;
   bool uses_rolling_key = false;
   bool randomized_sections = false;
+  bool packed = false;
+  bool chain_dispatch = false;
+  int confidence = 0;
+  int push_call_enters = 0;
+  int handler_insns = 0;
+  double entropy = 0;
+  std::string why;
+  std::string ep_sig;
   std::vector<std::string> vmp_sections;
 };
 
